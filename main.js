73649f73e24874a3ba11038ebcb16a1b114bf372
MichaelCapo23 <123pie23@gmail.com>
2018-11-20 10:06:28 -0800

    html/css/domcreate

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(startApp);
+
+function startApp() {
+    createDomBoard(dummyBoard());
+}
+
+function dummyBoard() {
+    return Array(10).fill(0).map(row => Array(10).fill(0));
+}
+
+function createDomBoard(board) {
+    board.forEach((row, y) => {
+        row.forEach((value, x) => {
+            const cell = $('<div>').addClass('' + y + x).addClass("cell");
+            $('.boardContainer').append(cell);
+        })
+    })
+}
+
+
